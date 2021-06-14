98ddd27d319cd6599f7f126f6d3b1bce892b8faa
wulabing <wulabing@gmail.com>
2021-06-14 13:15:40 +0800

    1.1.7.2
    [fix] 修复 ACME 申请证书上游切换默认CA导致的证书无法签发问题

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ Error="${Red}[错误]${Font}"
 Warning="${Red}[警告]${Font}"
 
 # 版本
-shell_version="1.1.7.1"
+shell_version="1.1.7.2"
 shell_mode="None"
 github_branch="dev"
 version_cmp="/tmp/version_cmp.tmp"
@@ -447,7 +447,9 @@ ssl_install() {
     fi
     judge "安装 SSL 证书生成脚本依赖"
 
-    curl https://get.acme.sh | sh -s email=my@example.com
+    read -rp "请输入用于注册域名的邮箱:" domain_email
+
+    curl https://get.acme.sh | sh -s email=$domain_email
     judge "安装 SSL 证书生成脚本"
 }
 domain_check() {
