27264fcde9ee13d119f2b7624045671852c14c10
玖柒Max <60207794+jiuqi9997@users.noreply.github.com>
2022-07-10 18:16:48 +0800

    更新API (#42)

--- install.sh
+++ install.sh
@@ -457,7 +457,7 @@ domain_check() {
     read -rp "请输入你的域名信息(eg:www.wulabing.com):" domain
     domain_ip=$(ping "${domain}" -c 1 | sed '1{s/[^(]*(//;s/).*//;q}')
     echo -e "${OK} ${GreenBG} 正在获取 公网ip 信息，请耐心等待 ${Font}"
-    local_ip=$(curl https://api-ipv4.ip.sb/ip)
+    local_ip=$(curl -s4L api.ipify.org)
     echo -e "域名dns解析IP：${domain_ip}"
     echo -e "本机IP: ${local_ip}"
     sleep 2
