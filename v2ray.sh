772bbdc1638228fa2aa085c67b7aa0a51564a1ff
Filterrr <36611101+Filterrr@users.noreply.github.com>
2022-09-07 18:29:18 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -391,7 +391,7 @@ CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 Environment=V2RAY_LOCATION_ASSET=/usr/local/lib/v2ray/
-ExecStart=/usr/local/bin/v2ray -config /etc/v2ray/config.json
+ExecStart=/usr/local/bin/v2ray run -c /etc/v2ray/config.json
 Restart=on-failure
 
 [Install]
@@ -408,7 +408,7 @@ CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 Environment=V2RAY_LOCATION_ASSET=/usr/local/lib/v2ray/
-ExecStart=/usr/local/bin/v2ray -config /etc/v2ray/%i.json
+ExecStart=/usr/local/bin/v2ray run -c /etc/v2ray/%i.json
 Restart=on-failure
 
 [Install]
